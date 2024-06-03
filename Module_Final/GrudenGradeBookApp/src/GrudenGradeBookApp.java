76972a38cf22ac9df3d9b8350364d91cf3eee3c0
Tricia <Tricia.Gruden@yahoo.com>
2024-06-02 21:35:10 -0400

    Final With Changes

--- Module_Final/GrudenGradeBookApp/src/GrudenGradeBookApp.java
+++ Module_Final/GrudenGradeBookApp/src/GrudenGradeBookApp.java
@@ -1,3 +1,21 @@
+
+/* Citations 
+
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th
+ed.). Pearson Education, Inc.
+Modifications by R. Krasso, 2021
+
+Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all
+rights reserved
+
+Java classes and objects. (n.d.-b). https://www.w3schools.com/
+
+*/
+
+
+
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
